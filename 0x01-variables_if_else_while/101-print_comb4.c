f1926676742c09d11561b0ff03f638dd130e1da6
SorenAcevedo <sorenacevedo@gmail.com>
2020-09-18 16:10:34 +0000

    101-print_comb4

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+/**
+* main - Don't repeat
+*
+* Return: 0
+*/
+int main(void)
+{
+	int num;
+	int num_2;
+	int num_3;
+
+	for (num = 48; num < 56; num++)
+	{
+		for (num_2 = 48; num_2 < 57; num_2++)
+		{
+			for (num_3 = 48; num_3 < 58; num_3++)
+			{
+				if (num >= num_2 || num_2 >= num_3 )
+					continue;
+				else
+				{
+					putchar(num);
+					putchar(num_2);
+					putchar(num_3);
+
+					if (num == 55 && num_2 == 56 && num_3 == 57)
+						continue;
+					else
+					{
+						putchar(',');
+						putchar(' ');
+					}
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
