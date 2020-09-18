4b3fdbcff8d047930901cfc728f6b81499f85e54
SorenAcevedo <70923936+SorenAcevedo@users.noreply.github.com>
2020-09-18 10:08:41 -0500

    Create 100-print_comb3.c

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+/**
+* main - Don't repeat 
+*
+* Return: 0
+*/
+int main()
+{
+  int num;
+  int num_2;
+  
+  for (num = 48, num < 57, num++)
+  {
+    for (num_2 = 48, num_2 < 58, num_2++)
+    {
+      if (num >= num_2)
+        continue;
+      else
+      {
+        putchar(num)
+        putchar(num_2)
+        
+        if (num == 56 && num_2 == 57)
+          continue;
+        else
+        {
+          putchar(',')
+          putchar(' ')
+        }
+      }
+    }
+    putchar('\n')
+  }
+}
