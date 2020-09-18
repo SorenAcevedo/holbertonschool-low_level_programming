4e2b77277e124f5d46a4c5fcdab757d1692bc86e
SorenAcevedo <sorenacevedo@gmail.com>
2020-09-18 04:34:43 +0000

    Create 8-print_base16.c

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+/**
+* main - while oricedure
+*
+* Return: 0
+*/
+int main(void)
+{
+	int letra = 48;
+
+	while (letra < 58)
+	{
+		putchar(letra);
+		letra = letra + 1;
+	}
+
+	letra = 97;
+
+	while (letra < 103)
+	{
+		putchar(letra);
+		letra = letra + 1;
+	}
+	putchar('\n');
+	return (0);
+}
