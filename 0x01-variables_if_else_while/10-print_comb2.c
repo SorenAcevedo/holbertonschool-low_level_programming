1d4a7eef43550a68932aad120fcf1ef7b0ef6717
SorenAcevedo <70923936+SorenAcevedo@users.noreply.github.com>
2020-09-18 00:00:11 -0500

    Create 10-print_comb2.c

---
+++ 0x01-variables_if_else_while/10-print_comb2.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+/**
+* main - while oricedure
+*
+* Return: 0
+*/
+int main(void)
+{
+	int num;
+  int num_2;
+  
+	for (num = 48; num < 58; num++)
+	{
+    for (num_2 = 48; num_2 < 58; num_2++)
+		{
+      putchar(num);
+      putchar(num_2);
+      if (num != 57 && num_" != 57)
+        {
+          putchar(',');
+          putchar(' ');
+        }
+    }
+	}
+	putchar('\n');
+	return (0);
+}
