c2a4451ce1f1f78e67a056270c51856dba7cb846
SorenAcevedo <sorenacevedo@gmail.com>
2020-09-18 03:25:21 +0000

    Third point

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+* main - while
+*
+*
+*/
+int main(void)
+{
+        int letra = 65;
+
+        while (letra < 65) {
+                putchar(letra);
+                putchar("\n");
+                letra = letra +1;
+        }
+}
