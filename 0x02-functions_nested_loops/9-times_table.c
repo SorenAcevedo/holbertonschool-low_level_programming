6f84741f40fc0dc874a0cb4aec6929c3d6487a6f
SorenAcevedo <sorenacevedo@gmail.com>
2020-09-22 16:03:54 +0000

    Correction 9-times_table.c

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -13,8 +13,22 @@ void times_table(void)
 		for (j = 0; j < 10; j++)
 		{
 			k = i * j;
-			_putchar((k / 10) + 48);
-			_putchar((k % 10) + 48);
+
+			if (j == 0)
+				_putchar((k % 10) + 48);
+
+			else if ((k / 10) == 0 && j != 0)
+			{
+				_putchar(' ');
+				_putchar((k % 10) + 48);
+			}
+
+			else
+			{
+				_putchar((k / 10) + 48);
+				_putchar((k % 10) + 48);
+			}
+
 			if (j != 9)
 			{
 				_putchar(',');
