6f2a58764a7e556117bc68fb854776f17b1985d7
SorenAcevedo <sorenacevedo@gmail.com>
2020-09-22 16:12:17 +0000

    Create my 10-add.c

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+* add - add 2 numbers
+* @a: one number
+*
+* @b: the other number
+*
+* Return: add
+*/
+int add(int a, int b)
+{
+	int c;
+
+	c = a + b;
+
+	return (c);
+}
