fa9dd6ba6238de4e307900499269b0a382aa81f3
SorenAcevedo <sorenacevedo@gmail.com>
2020-09-23 03:46:35 +0000

    Create my 102-fibonacci

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+* main - Fibonnacci 50
+* Return: 0
+*/
+
+int main(void)
+{
+	int i, j = 0, k = 1, suma = 0;
+
+	for (i = 0; i < 50; i++)
+	{
+		suma = j + k;
+		j = k;
+		k = suma;
+
+		printf("%d", suma);
+		if (i != 49)
+			printf(", ");
+	}
+	printf("\n");
+	return (0);
+}
