cfa8705242214a517d64950669b301859c969f98
SorenAcevedo <sorenacevedo@gmail.com>
2020-09-23 20:06:45 +0000

    Correction with betty of the crazy 104-fibonacci.c

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -10,7 +10,7 @@ int main(void)
 	int i;
 	unsigned long j = 0, k = 1, suma = 0, a, b, c, d, first_digits, last_3;
 
-	for (i = 0; i < 91; i++) // El rango del tipo "lu" abarca hasta el valor 92 en fibonacci
+	for (i = 0; i < 91; i++) /* Tipo "lu" abarca hasta el valor 92 en fibo*/
 	{
 		suma = j + k;
 		j = k;
@@ -19,15 +19,15 @@ int main(void)
 		printf("%ld", suma);
 		printf(", ");
 	}
-	a = j / 1000; // Número 91 en fibonacci sin últimos 3 digito
-	b = j % 1000; // Últimos 3 digitos de fibonacci 
-	c = suma / 1000; // Número 92 en fibonacci sin últimos 3 digito
-	d = suma % 1000; // Últimos 3 digitos de número 92 fibonacci
+	a = j / 1000; /* Número 91 en fibonacci sin últimos 3 digito*/
+	b = j % 1000; /* Últimos 3 digitos de fibonacci */
+	c = suma / 1000; /* Número 92 en fibonacci sin últimos 3 digito */
+	d = suma % 1000; /* Últimos 3 digitos de número 92 fibonacci */
 
 	for (i = 0; i < 8; i++)
 	{
 		last_3 = (b + d) % 1000;
-		
+
 		if (last_3 > 999)
 			first_digits = a + c + 1;
 		else
