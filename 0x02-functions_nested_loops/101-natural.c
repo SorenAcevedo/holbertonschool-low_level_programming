821dd03014a5f0bccac481a5ba0e2cdb01fe1b4b
SorenAcevedo <sorenacevedo@gmail.com>
2020-09-23 03:25:39 +0000

    push

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -7,13 +7,13 @@
 
 int main(void)
 {
-	int i, suma;
+	int i, suma = 0;
 
 	for (i = 0; i < 1024; i++)
 	{
 		if ((i % 3) == 0 || (i % 5) == 0)
 			suma += i;
 	}
-	printf("%d", suma);
+	printf("%d\n", suma);
 	return (0);
 }
