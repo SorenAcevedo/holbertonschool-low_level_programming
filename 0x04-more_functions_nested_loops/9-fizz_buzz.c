993f441921e72db4b6bdc858da7d9034358cbe77
SorenAcevedo <sorenacevedo@gmail.com>
2020-09-24 16:26:43 +0000

    Create 9-fizz_buzz.c with related files

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+* main - print num 0 - 99, %3 fizz, %3 & %5 buzz
+* Return: (0)
+*/
+
+int main(void)
+{
+	int i;
+
+	for (i = 1; i < 101; i++)
+		if ((i % 3 == 0) && (i % 5 == 0))
+			printf("FizzBuzz ");
+		else if (i % 5 == 0)
+			printf("Buzz ");
+		else if (i % 3 == 0)
+			printf("Fizz ");
+		else
+			printf("%d ", i);
+	printf("\n");
+	return (0);
+}
