81f8bb6d8346fe062710300978be2e8da4a78c50
SorenAcevedo <sorenacevedo@gmail.com>
2020-09-24 15:17:56 +0000

    Create the 5-more_numbers.c with related files

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,22 @@
+#include "holberton.h"
+/**
+* void more_numbers - 0 - 14 -> 10 times
+*
+*/
+void more_numbers(void)
+{
+	int i, j, l;
+
+	for (i = 0; i < 10; i++)
+	{
+		for (j = 0; j < 15; j++)
+		{
+			l = j % 10;
+			if (j > 9)
+				_putchar(49);
+
+			_putchar(l + 48);
+		}
+		_putchar(10);
+	}
+}
