aa2d05ceee78c7e4e35f44cea22abab87d89933e
SorenAcevedo <sorenacevedo@gmail.com>
2020-09-24 15:34:05 +0000

    Create 6-print_line.c with related files

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,16 @@
+#include "holberton.h"
+/**
+* print_line - print _
+* @n: int -> char ascii
+*/
+void print_line(int n)
+{
+	int i;
+
+	if (n > 0)
+	{
+		for (i = 0; i <= n; i++)
+			_putchar('_');
+	}
+	_putchar(10);
+}
