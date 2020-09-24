a92d08a426e28408294cceff2ef516c29e4abc80
SorenAcevedo <sorenacevedo@gmail.com>
2020-09-24 14:59:05 +0000

    Create the 3-print_numbers.c with the related files

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,15 @@
+#include "holberton.h"
+/**
+* mul - mult 2 numbers
+* @a: an integer
+* @b: other integer
+* Return: mul.
+*/
+int mul(int a, int b)
+{
+	int mul;
+
+	mul = a * b;
+
+	return (mul);
+}
