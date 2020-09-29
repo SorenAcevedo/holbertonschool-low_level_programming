772a9239a2ffb498fe059645f011f48a2dd64ff0
SorenAcevedo <sorenacevedo@gmail.com>
2020-09-29 18:03:06 +0000

    Create 09 with related files

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,19 @@
+#include "holberton.h"
+/**
+* _strcpy - copies string in the dest
+* @dest: destino
+* @src: fuente
+*/
+char *_strcpy(char *dest, char *src)
+{
+	int i, l;
+
+	l = 1;
+
+	while (src[l] != '\0')
+		l++;
+
+	for (i = 0; i < l; i++)
+		dest[i] = src[i];
+	return (dest);
+}
