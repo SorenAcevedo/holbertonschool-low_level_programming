93ad1084b075bd74bc52b4824b5954506f7643e4
SorenAcevedo <sorenacevedo@gmail.com>
2020-10-01 15:53:59 +0000

    Correction about 0, and create 1 with related files

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -3,16 +3,17 @@
 * _strcat - concatenate 2 str
 * @dest: str 1
 * @src: str 2
+* @n: n from src
 * Return: final str
 */
-char *_strcat(char *dest, char *src)
+char *_strncat(char *dest, char *src, int n)
 {
 	int i, j;
 
 	for (i = 0; dest[i] != 0; i++)
 	{
 	}
-	for (j = 0; src[j] != 0; j++)
+	for (j = 0; j < n && src[j] != 0; j++)
 		dest[i + j] = src[j];
 	return (dest);
 }
