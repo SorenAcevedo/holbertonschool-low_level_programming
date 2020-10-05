1794d50e5762187aebfde31412af1af7ce374dc7
SorenAcevedo <sorenacevedo@gmail.com>
2020-10-05 17:05:51 +0000

    Create 05 file, string in a string

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,34 @@
+#include "holberton.h"
+
+/**
+* _strstr - locates a string in a string
+* @haystack: str
+* @needle: str
+* Return: a pointer the 1 occurrence str in the string s,
+* or NULL if the str is not found
+*/
+
+char *_strstr(char *haystack, char *needle)
+{
+	unsigned int i, j, k, stop = 0;
+	char *p;
+
+	p = '\0';
+
+	for (k = 0; needle[k] != 0; k++)
+	{
+	}
+
+	for (i = 0; haystack[i] != 0 && stop == 0; i++)
+	{
+		for (j = 0; needle[j] == haystack[i + j]; j++)
+		{
+			if (j == k)
+			{
+				p = &(haystack[i]);
+				stop = 1;
+			}
+		}
+	}
+	return (p);
+}
