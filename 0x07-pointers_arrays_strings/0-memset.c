ca77e8329f6436902b0508582215e77da601d3b5
SorenAcevedo <sorenacevedo@gmail.com>
2020-10-05 15:35:43 +0000

    Create 0 file, memset

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,20 @@
+#include "holberton.h"
+
+/**
+* _memset - set values since a adress to adress + n
+* @s: pointer
+* @b: value
+* @n: number
+* Return: a pointer to the memory area
+*/
+
+char *_memset(char *s, char b, unsigned int n)
+{
+	unsigned int i;
+
+	for (i = 0; i < n; i++)
+	{
+		*(s + i) = b;
+	}
+	return(s);
+}
