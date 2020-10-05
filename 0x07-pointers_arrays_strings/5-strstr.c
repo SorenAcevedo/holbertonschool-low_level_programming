3b8280dd2e8b15a68f93a50c37ce8ba6f5bab5f5
SorenAcevedo <sorenacevedo@gmail.com>
2020-10-05 20:31:20 +0000

    Correction 05

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -21,7 +21,7 @@ char *_strstr(char *haystack, char *needle)
 
 	for (i = 0; haystack[i] != 0 && stop == 0; i++)
 	{
-		for (j = 0; needle[j] == haystack[i + j]; j++)
+		for (j = 0; needle[j] == haystack[i + j] && needle[j] != 0; j++)
 		{
 			if (j == (k - 1) && k > 0)
 			{
