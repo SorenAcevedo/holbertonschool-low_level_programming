4b4a532e37b830c07a012db88bd74857693489c0
SorenAcevedo <sorenacevedo@gmail.com>
2020-10-05 16:06:01 +0000

    Create 03 file, gets the length of a prefix substring

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,26 @@
+#include "holberton.h"
+
+/**
+* _strspn - gets the length of a prefix substring
+* @s: str
+* @accept: char
+* Return: number of bytes
+*/
+
+unsigned int _strspn(char *s, char *accept)
+{
+	unsigned int i, j, con = 1;
+
+	for (i = 0; accept[i] != 0; i++)
+	{
+		for (j = 0; s[j] != 0; j++)
+		{
+			if (accept[i] == s[j])
+			{
+				con++;
+				break;
+			}
+		}
+	}
+	return (con);
+}
