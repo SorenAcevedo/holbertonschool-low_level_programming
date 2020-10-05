c1903534a048d18dd0d3f15617fa794f32801d8c
SorenAcevedo <sorenacevedo@gmail.com>
2020-10-05 19:54:41 +0000

    Correction 03

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -11,16 +11,18 @@ unsigned int _strspn(char *s, char *accept)
 {
 	unsigned int i, j, con = 0;
 
-	for (i = 0; accept[i] != 0; i++)
+	for (i = 0; s[i] != 0; i++)
 	{
 		for (j = 0; s[j] != 0; j++)
 		{
-			if (accept[i] == s[j])
+			if (accept[j] == s[i])
 			{
 				con++;
 				break;
 			}
 		}
+		if (accept[j] != s[i])
+			break;
 	}
 	return (con);
 }
