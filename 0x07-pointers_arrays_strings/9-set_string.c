f4c3416178650de8c370cffa4f324d63cb422a8b
SorenAcevedo <sorenacevedo@gmail.com>
2020-10-05 21:12:54 +0000

    Betty correction 09

--- 0x07-pointers_arrays_strings/9-set_string.c
+++ 0x07-pointers_arrays_strings/9-set_string.c
@@ -5,7 +5,7 @@
 * @s: pointer to pointer
 * @to: pointer to string
 * This is to pass the value of the variable in the pointer *to
-* called s0, to the value s1 in the pointer **s 
+* called s0, to the value s1 in the pointer **s
 */
 
 void set_string(char **s, char *to)
