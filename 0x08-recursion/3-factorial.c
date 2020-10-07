843e6ad44c65ed2e042876f25d326561a2e82e7b
SorenAcevedo <sorenacevedo@gmail.com>
2020-10-07 17:06:57 +0000

    Create 03 with sources help

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,19 @@
+#include "holberton.h"
+
+/**
+* factorial - factorial product
+* @n: number
+* Return: factorial product
+*/
+
+int factorial(int n)
+{
+	if (n < 0)
+		return (-1);
+	else if (n <= 1)
+	{
+		return (1);
+	}
+	else
+		return (n * factorial(n - 1));
+}
