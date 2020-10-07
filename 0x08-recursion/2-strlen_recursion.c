4a080413acd088a0a8bde66bcb02a03f8dbcb6a9
SorenAcevedo <sorenacevedo@gmail.com>
2020-10-07 16:47:21 +0000

    Create 02 with related files

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,18 @@
+#include "holberton.h"
+
+/**
+* _strlen_recursion - recursion, len of a string
+* @s: string
+*/
+
+int _strlen_recursion(char *s)
+{
+	int len = 0;
+
+	if (*s != 0)
+	{
+		len = _strlen_recursion(s + 1);
+		len ++; 
+	}
+	return (len);
+}
