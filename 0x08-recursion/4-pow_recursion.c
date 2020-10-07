a229f628094a0e12560682b838046b761f0b91e7
SorenAcevedo <sorenacevedo@gmail.com>
2020-10-07 17:33:52 +0000

    Correction 02

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,22 @@
+#include "holberton.h"
+
+/**
+* _pow_recursion - num pow
+* @x: num
+* @y: pow
+* Return: num pow
+*/
+
+int _pow_recursion(int x, int y)
+{
+	int pow = 1;
+
+	if (y < 0)
+		return (-1);
+	else if (y == 0)
+		return (1);
+	else
+	{
+		pow = _pow_recursion(int x, int y)
+	}
+}
