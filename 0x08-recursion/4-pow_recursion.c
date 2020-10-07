e46a489bb0141ae095734d12a0ace8b57be77259
SorenAcevedo <sorenacevedo@gmail.com>
2020-10-07 17:58:03 +0000

    Create 04 with related files

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -9,14 +9,10 @@
 
 int _pow_recursion(int x, int y)
 {
-	int pow = 1;
-
 	if (y < 0)
 		return (-1);
 	else if (y == 0)
 		return (1);
 	else
-	{
-		pow = _pow_recursion(int x, int y)
-	}
+		return (x * _pow_recursion(x, y - 1));
 }
