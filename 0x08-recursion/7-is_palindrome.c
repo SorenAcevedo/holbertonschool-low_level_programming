bb0c9b74c3de5c90f02adc058e59f9fcac13618d
SorenAcevedo <sorenacevedo@gmail.com>
2020-10-08 00:17:27 +0000

    Correction 07

--- 0x08-recursion/7-is_palindrome.c
+++ 0x08-recursion/7-is_palindrome.c
@@ -18,6 +18,29 @@ int _strlen_recursion(char *s)
 }
 
 /**
+* oposite - compare oposite position
+* @s: string
+* @i: iterator
+* Return: 1 if is polindr or 0 if no
+*/
+
+int oposite(char *s, int i)
+{
+	int l;
+	char c;
+
+	l = _strlen_recursion(s);
+	c = s[l - i];
+
+	if (c != *s)
+		return (0);
+	else if (c == *s)
+		return (1);
+	else
+		return (oposite(s + 1, i + 1));
+}
+
+/**
 * is_palindrome - string is palindrome or no
 * @s: string
 * Return: if is palindrome or not
@@ -25,19 +48,5 @@ int _strlen_recursion(char *s)
 
 int is_palindrome(char *s)
 {
-	int l;
-	char c;
-
-	l = _strlen_recursion(s);
-
-	c = s[l - 1];
-
-	if (l < 2)
-		return (1);
-	else if (c != *s)
-		return (0);
-	else if (c == *s)
-		return (1);
-	else
-		return (is_palindrome(s + 1));
+	return (oposite(s, 1));
 }
