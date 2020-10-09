a3026a4a81a80757f64259c415f0263f15a20b2a
SorenAcevedo <sorenacevedo@gmail.com>
2020-10-09 15:53:33 +0000

    Create 00, understand how compile with unused parameters

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+* main - first parameter
+* @argc: num of parameters
+* @argv: str with str parameteres (like a doble pointer)
+* Return: 0.
+*/
+
+int main (int __attribute__((unused))argc , char *argv[])
+{
+	printf("%s\n", argv[0]);
+	return (0);
+}
