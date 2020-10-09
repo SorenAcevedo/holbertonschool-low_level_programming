4bdf911e12379b77684a3d007a905c897ac396e2
SorenAcevedo <sorenacevedo@gmail.com>
2020-10-09 15:59:40 +0000

    Create 01 with related files

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+* main - first parameter
+* @argc: num of parameters
+* @argv: str with str parameteres (like a doble pointer)
+* Return: 0.
+*/
+
+int main(int argc, char __attribute__((unused))*argv[])
+{
+	printf("%d\n", argc - 1);
+	return (0);
+}
