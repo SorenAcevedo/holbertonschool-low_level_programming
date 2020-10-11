9abc654b83d683a4a60b4f56a3d73a473651934a
SorenAcevedo <sorenacevedo@gmail.com>
2020-10-11 00:20:21 +0000

    Correction about 5

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -1,25 +1,5 @@
 #include <stdio.h>
-
-/**
-* str_to_int - pass string to int
-* @s: str
-* Return: int
-*/
-
-int str_to_int(char *s)
-{
-	int k = 0, entero = 0;
-
-	if (s[0] == '-')
-		k = 1;
-	for (; s[k] != 0; k++)
-	{
-		entero = entero * 10 + (s[k] - 48);
-	}
-	if (s[0] == '-')
-		entero *= -1;
-	return (entero);
-}
+#include <stdlib.h>
 
 /**
 * main - first parameter
@@ -30,14 +10,14 @@ int str_to_int(char *s)
 
 int main(int argc, char *argv[])
 {
-	int i, j = 0, stop = 0;
+	int i, j = 0;
 
-	if (argc > 1 && stop == 0)
+	if (argc > 1)
 	{
 		for (i = 1; i < argc; i++)
 		{
 			if ((*argv[i] > 47 && *argv[i] < 58) || *argv[i] == '-')
-				j += str_to_int(argv[i]);
+				j += atoi(argv[i]);
 			else
 				break;
 		}
