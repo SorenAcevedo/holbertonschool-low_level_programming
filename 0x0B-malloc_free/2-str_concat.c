a42d3636da5700b8bbdbe83d76b2a8be8ee78d02
SorenAcevedo <sorenacevedo@gmail.com>
2020-10-13 21:24:28 +0000

    Create 03 grid

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -13,17 +13,12 @@ char *str_concat(char *s1, char *s2)
 	char *p;
 
 	/* primero asigno el tamaño de memoria con Malloc */
-	if (s1 != NULL)
-	{
-		while (s1[l1] != 0)
-			l1++;
-	}
-	if (s2 != NULL)
-	{
-		while (s2[l2] != 0)
-			l2++;
-	}
-
+		
+	while (s1[l1] != 0)
+		l1++;	
+	while (s2[l2] != 0)
+		l2++;
+	
 	l = l1 + l2 + 1;
 
 	p = malloc(sizeof(char) * l);
