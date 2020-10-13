173bf28515b85ba38eb104f6fbd406e56402e8df
SorenAcevedo <sorenacevedo@gmail.com>
2020-10-13 21:03:37 +0000

    Create 03 grid

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -23,13 +23,13 @@ char *str_concat(char *s1, char *s2)
 	if (l == 0)
 		return ("");
 
-	p = malloc(l * sizeof(char));
+	p = malloc(l);
 
 	for (i = 0; s1[i] != 0; i++)
 		p[i] = s1[i];
 	for (j = 0; s2[j] != 0; j++)
 		p[i + j] = s2[j];
-	p[i + j] = 0;
+	p[i + j] = '\0';
 
 	return (p);
 }
