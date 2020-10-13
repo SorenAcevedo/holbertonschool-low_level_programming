b59548ff96782b7a77608c4c37592c2f078112f0
SorenAcevedo <sorenacevedo@gmail.com>
2020-10-13 19:12:15 +0000

    Correction 02

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -9,12 +9,15 @@
 
 char *str_concat(char *s1, char *s2)
 {
-	unsigned int i, j, l, l1, l2;
+	unsigned int i, j, l, l1 = 0, l2 = 0;
 	char *p;
 
 	/* primero asigno el tamaño de memoria con Malloc */
-	l1 = sizeof(s1);
-	l2 = sizeof(s2);
+	while (s1[l1] != 0)
+		l1++;
+	while (s2[l2] != 0)
+		l2++;
+
 	l = l1 + l2;
 
 	if (l == 0)
