9ad906fd84d6457c2ffc93476cccb9443347512d
SorenAcevedo <sorenacevedo@gmail.com>
2020-10-13 15:46:20 +0000

    Create 00 with related file

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -1,9 +1,26 @@
+#include <stdlib.h>
+
 /**
 * create_array - with DM
-* @size
-* @c 
+* @size: size of a string
+* @c: char
+* Return: Null if size 0
 */
 
 char *create_array(unsigned int size, char c)
 {
+	unsigned int i;
+	char *p;
+
+	/* primero asigno el tamaño de memoria con Malloc */
+
+	p = (malloc(size));
+
+	if (size == 0)
+		return (0);
+
+	for (i = 0; i < size; i++)
+		p[i] = c;
+
+	return (p);
 }
