a632b8810d73a68eceba8eb4612c665fd9c98dff
SorenAcevedo <sorenacevedo@gmail.com>
2020-10-13 23:14:20 +0000

    Create the 05 with related file

---
+++ 0x0B-malloc_free/5-argstostr.c
@@ -0,0 +1,44 @@
+#include <stdlib.h>
+
+/**
+* argstostr - sdfa
+* @ac: matriz
+* @av: dimension y
+* Return: Null if size 0
+*/
+
+char *argstostr(int ac, char **av)
+{
+	int i, j, k = 0, len = 0;
+	char *p, *var;
+
+	if (ac == 0 || av == NULL)
+		return (NULL);
+
+	for (i = 0; i < ac; i++)
+	{
+		var = av[i];
+		for (j = 0; var[j] != 0; j++)
+		{
+			len++;
+		}
+	}
+
+	p = malloc(len + ac);
+
+	if (p == NULL)
+		return (NULL);
+
+	for (i = 0; i < ac; i++)
+	{
+		var = av[i];
+		for (j = 0; var[j] != 0; j++)
+		{
+			p[k] = var[j];
+			k++;
+		}
+		p[k] = 10;
+		k++;
+	}
+	return (p);
+}
