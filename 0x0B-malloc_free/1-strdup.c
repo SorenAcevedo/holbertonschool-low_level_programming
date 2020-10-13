d32ebfeaa42a68d5ce4f6863e67756e65f339ce4
SorenAcevedo <sorenacevedo@gmail.com>
2020-10-13 17:41:20 +0000

    Correction 01

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -15,10 +15,11 @@ char *_strdup(char *str)
 
 	p = malloc(sizeof(str) + 1);
 
-	if (p == NULL || str == NULL)
-		return (NULL);
-
-	for (j = 0; p[j] != 0; j++)
-		p[j] = str[j];
-	return (p);
+	if (p != NULL || sizeof(str) != 0)
+	{
+		for (j = 0; p[j] != 0; j++)
+			p[j] = str[j];
+		return (p);
+	}
+	return (NULL);
 }
