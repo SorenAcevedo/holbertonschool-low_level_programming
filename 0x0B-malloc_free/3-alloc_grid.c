c787bcd34e6345b8f07f82ec5fafaee2f4cb0dc9
SorenAcevedo <sorenacevedo@gmail.com>
2020-10-13 19:43:13 +0000

    Create 03 grid

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -9,16 +9,20 @@
 
 int **alloc_grid(int width, int height)
 {
-	unsigned int i;
-	char **p, *c;
+	int i, j, k, **m;
 
-	/* primero asigno el tamaño de memoria con Malloc */
-	p = malloc(width * height);
-
-	for (i = 0; s1[i] != 0; i++)
-		p[i] = s1[i];
-	for (j = 0; s2[j] != 0; j++)
-		p[i + j] = s2[j];
-
-	return (p);
+	/* primero asigno el tamaño de memoria para las filas*/
+	m = (int **) malloc(height * sizeof(int *));
+	/* segundo asigno el tamaño para las columnas, fila por fila*/
+	for (i = 0; i < height; i++)
+		m[i] = (int *) malloc(width * sizeof(int));
+	/*Lleno las posiciones con 0*/
+	for (j = 0; j < height; j++)
+	{
+		for (k = 0; k < width; k++)
+		{
+			m[j][k] = 0;
+		}
+	}
+	return (m);
 }
