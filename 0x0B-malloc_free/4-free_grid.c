8e87875f14ae1505d781d81014e725c996ed816e
SorenAcevedo <sorenacevedo@gmail.com>
2020-10-13 22:18:31 +0000

    Create 04

--- 0x0B-malloc_free/4-free_grid.c
+++ 0x0B-malloc_free/4-free_grid.c
@@ -9,6 +9,11 @@
 
 void free_grid(int **grid, int height)
 {
-	
-	return (m);
+	int i, columnas;
+	/* Hallar el número de columnas*/
+	columnas = sizeof(grid) / height;
+
+	for (i = 0; i < columnas; i++)
+		free(grid[i]);
+	free(grid);
 }
