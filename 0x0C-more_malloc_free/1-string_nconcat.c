bb34ffc9c632d69683f7c15e8ce2ef4586c41214
SorenAcevedo <sorenacevedo@gmail.com>
2020-10-15 19:24:26 +0000

    Correction 01 with complete else

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -31,7 +31,7 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	if (n < l2)
 		p = malloc(sizeof(char) * (n + l1 + 1));
 	else
-	{	
+	{
 		p = malloc(sizeof(char) * l);
 		n = l2;
 	}
@@ -41,7 +41,7 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 
 	for (i = 0; i < l1; i++)
 		p[i] = s1[i];
-	for (j = 0; i < n; j++)
+	for (j = 0; j < n; j++)
 		p[i + j] = s2[j];
 	p[i + j] = 0;
 
