2299593be5f661b995765514c8aa86b33a84846f
SorenAcevedo <sorenacevedo@gmail.com>
2020-10-15 19:47:46 +0000

    Correction 02 with char pointer

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -8,15 +8,17 @@
 */
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
-	void *p;
+	char *p;
+	unsigned int i;
 
 	if (nmemb == 0 || size == 0)
-		return (NULL);
+	{
+		p = malloc(nmemb * size);
+		if (p != NULL)
+			for (i = 0; i < nmemb * size; i++)
+				p[i] = 0;
+			return (p);
+	}
 
-	p = malloc(nmemb * size);
-
-	if (p != NULL)
-		return (p);
-	
 	return (NULL);
 }
