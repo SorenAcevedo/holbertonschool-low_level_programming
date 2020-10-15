177fec9961d2e1079e6a8ed5bed80940dacc6182
SorenAcevedo <sorenacevedo@gmail.com>
2020-10-15 19:07:04 +0000

    Correction 01 with empty case

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -32,13 +32,14 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 		p = malloc(sizeof(char) * (n + l1 + 1));
 	else
 		p = malloc(sizeof(char) * l);
+		n = l2;
 
 	if (p == NULL)
 		return (NULL);
 
 	for (i = 0; i < l1; i++)
 		p[i] = s1[i];
-	for (j = 0; j < n; j++)
+	for (j = 0; i < n; j++)
 		p[i + j] = s2[j];
 	p[i + j] = 0;
 
