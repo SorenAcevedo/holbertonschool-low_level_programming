ef8151eab208a33e6a8e264de250413cfb6266d0
SorenAcevedo <sorenacevedo@gmail.com>
2020-10-15 19:58:21 +0000

    Correction 02 with related files

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -17,10 +17,11 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 	{
 		p = malloc(nmemb * size);
 		if (p != NULL)
+		{
 			for (i = 0; i < nmemb * size; i++)
 				p[i] = 0;
 			return (p);
+		}
 	}
-
 	return (NULL);
 }
