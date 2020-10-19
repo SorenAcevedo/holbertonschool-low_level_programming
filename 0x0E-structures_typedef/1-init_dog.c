440f897461e8e84e2939b32162fd82ad6974e103
SorenAcevedo <sorenacevedo@gmail.com>
2020-10-19 23:03:34 +0000

    Create main files

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,16 @@
+#include "dog.h"
+
+/**
+*
+*
+*
+*
+*
+*/
+
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	struct dog d my_dog.name = name;
+	my_dog.age = age;
+	my_dog.owner = owner;
+}
--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,5 +1,6 @@
 #ifndef DOG_H
 #define	DOG_H
+
 /**
 * struct dog - struct about dogs
 * @name: the name of the dog
@@ -7,11 +8,15 @@
 * @owner: the name of the owner
 */
 
-struct dog
-{
+struct dog {
 	char *name;
 	float age;
 	char *owner;
-};
+} dog_t;
+
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
 
 #endif
