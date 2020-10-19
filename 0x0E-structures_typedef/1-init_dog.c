fc8c808444b65d767419ca1d57943e39d567fa22
SorenAcevedo <sorenacevedo@gmail.com>
2020-10-19 23:20:55 +0000

    Correction with null conditional 01

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -1,16 +1,20 @@
 #include "dog.h"
+#include <stdlib.h>
 
 /**
-*
-*
-*
-*
-*
+* init_dog - create a new dog
+* @d: name of object
+* @name: name of the new dog
+* @age: age of the new dog
+* @owner: name of the owner
 */
 
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
-	struct dog d my_dog.name = name;
-	my_dog.age = age;
-	my_dog.owner = owner;
+	if (d != NULL)
+	{
+		d->name = name;
+		d->age = age;
+		d->owner = owner;
+	}
 }
