5d95a639c2147a0f8060cd967461cc6d999f1a97
SorenAcevedo <sorenacevedo@gmail.com>
2020-10-20 02:05:31 +0000

    Create 05

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,18 @@
+#include "dog.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+* free_dog - print atributos
+* @d: object dog
+*/
+
+void free_dog(struct dog *d)
+{
+	if (d != NULL)
+	{
+		free(d->name);		
+		free(d->owner);
+		free(d);
+	}
+}
