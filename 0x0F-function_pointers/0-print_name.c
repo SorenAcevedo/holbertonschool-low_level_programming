8f86d34ec372812afa73fb9be41386453bd25a96
SorenAcevedo <sorenacevedo@gmail.com>
2020-10-21 22:39:36 +0000

    Create the first point, understand how to use pointer to function

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+* print_name - print a name between the use of p to funct
+* @name: name to print
+* @f: pointer to function to use
+*/
+
+void print_name(char *name, void (*f)(char *))
+{
+	(*f)(name);
+}
