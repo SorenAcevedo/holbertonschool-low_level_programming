0a0126b537ad882712a6ab66ded6fb2b2b84cd02
SorenAcevedo <sorenacevedo@gmail.com>
2020-10-23 17:14:16 +0000

    Create 02 with related files

---
+++ 0x10-variadic_functions/2-print_strings.c
@@ -0,0 +1,33 @@
+#include <stdarg.h>
+#include <stdio.h>
+
+/**
+* print_strings - print str's separted by any separator
+* @separator: separator
+* @n: number of parameters
+*/
+
+void print_strings(const char *separator, const unsigned int n, ...)
+{
+	/*Crear un tipo va_list que va a tener el número de para*/
+	if (n != 0)
+	{
+		va_list argumentos;
+		unsigned int i;
+		char *cadena;
+
+		va_start(argumentos, n);
+
+		for (i = 0; i < n; i++)
+		{
+			cadena = va_arg(argumentos, char*);
+			if (separator != NULL && i != n - 1)
+				printf("%s%s ", cadena, separator);
+			else
+				printf("%s", cadena);
+		}
+		putchar (10);
+		/* Limpiar memoria */
+		va_end(argumentos);
+	}
+}
