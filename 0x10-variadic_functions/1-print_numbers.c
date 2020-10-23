8220ac1b81ce7d72539d1ec835b7d044c945ab15
SorenAcevedo <sorenacevedo@gmail.com>
2020-10-23 16:37:47 +0000

    Create 01 with related file

---
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -0,0 +1,33 @@
+#include <stdarg.h>
+#include <stdio.h>
+
+/**
+* print_numbers - print nums separted by any separator
+* @separator: separator
+* @n: number of parameters
+*/
+
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+	/*Crear un tipo va_list que va a tener el número de para*/
+	if (n != 0)
+	{
+		va_list argumentos;
+		unsigned int i;
+		int number;
+
+		va_start(argumentos, n);
+
+		for (i = 0; i < n; i++)
+		{
+			number = va_arg(argumentos, int);
+			if (separator != NULL && i != n - 1)
+				printf("%d%s ", number, separator);
+			else
+				printf("%d ", number);
+		}
+		putchar (10);
+		/* Limpiar memoria */
+		va_end(argumentos);
+	}
+}
