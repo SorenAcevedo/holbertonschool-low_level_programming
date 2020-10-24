d63fe872c95509c1e38e97b5f473e17bb6f5b19f
SorenAcevedo <sorenacevedo@gmail.com>
2020-10-24 17:57:28 +0000

    Correction 01

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -10,23 +10,21 @@
 void print_numbers(const char *separator, const unsigned int n, ...)
 {
 	/*Crear un tipo va_list que va a tener el número de para*/
-	if (n != 0)
+	va_list argumentos;
+	unsigned int i;
+	int number;
+
+	va_start(argumentos, n);
+
+	for (i = 0; i < n; i++)
 	{
-		va_list argumentos;
-		unsigned int i;
-		int number;
-
-		va_start(argumentos, n);
-
-		for (i = 0; i < n; i++)
-		{
-			number = va_arg(argumentos, int);
-			printf("%d", number);
-			if (separator != NULL && i != (n - 1))
-				printf("%s", separator);
-		}
-		/* Limpiar memoria */
-		va_end(argumentos);
-		printf("\n");
+		number = va_arg(argumentos, int);
+		printf("%d", number);
+		if (separator != NULL && i != (n - 1))
+			printf("%s", separator);
+	}
+	putchar(10);
+	/* Limpiar memoria */
+	va_end(argumentos);
 	}
 }
