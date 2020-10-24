0b1befe8572acd2934f9df4d28f2bd2077f0874c
SorenAcevedo <sorenacevedo@gmail.com>
2020-10-24 17:38:37 +0000

    Correction 02

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -21,7 +21,7 @@ void print_strings(const char *separator, const unsigned int n, ...)
 		for (i = 0; i < n; i++)
 		{
 			cadena = va_arg(argumentos, char*);
-			if (cadena == NULL)
+			if (cadena == 0)
 				cadena = "(nil)";
 			if (separator && i < n - 1)
 				printf("%s%s", cadena, separator);
