46b60771edad2aa024481e1297c1d9422f3d8624
SorenAcevedo <sorenacevedo@gmail.com>
2020-10-24 18:00:46 +0000

    Correction 02

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -10,26 +10,23 @@
 void print_strings(const char *separator, const unsigned int n, ...)
 {
 	/*Crear un tipo va_list que va a tener el número de para*/
-	if (n != 0)
+	va_list argumentos;
+	unsigned int i;
+	char *cadena;
+
+	va_start(argumentos, n);
+
+	for (i = 0; i < n; i++)
 	{
-		va_list argumentos;
-		unsigned int i;
-		char *cadena;
-
-		va_start(argumentos, n);
-
-		for (i = 0; i < n; i++)
-		{
-			cadena = va_arg(argumentos, char*);
-			if (cadena == 0)
-				cadena = "(nil)";
-			if (separator && i < n - 1)
-				printf("%s%s", cadena, separator);
-			else
-				printf("%s", cadena);
-		}
-		putchar (10);
-		/* Limpiar memoria */
-		va_end(argumentos);
+		cadena = va_arg(argumentos, char*);
+		if (cadena == 0)
+			cadena = "(nil)";
+		if (separator && i < n - 1)
+			printf("%s%s", cadena, separator);
+		else
+			printf("%s", cadena);
 	}
+	putchar (10);
+	/* Limpiar memoria */
+	va_end(argumentos);
 }
