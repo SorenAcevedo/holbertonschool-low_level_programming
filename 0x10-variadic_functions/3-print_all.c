72b0693122f85954ac712b82eb0da2428f96ab67
SorenAcevedo <sorenacevedo@gmail.com>
2020-10-24 18:38:05 +0000

    Correction 03

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -7,43 +7,43 @@
 
 void print_all(const char * const format, ...)
 {
-	va_list argumentos;
-	int j = 0, len = 0;
-	char *s;
-
-	va_start(argumentos, format);
-
-	while (format[j] != 0)
+	while (format)
 	{
-		switch (format[j])
+		va_list argumentos;
+		int j = 0, i;
+		char *s;
+
+		va_start(argumentos, format);
+		while (format[j] != 0)
 		{
-			case 'c':
-				printf("%c", va_arg(argumentos, int));
-				len = 1;
-				break;
-			case 'i':
-				printf("%i", va_arg(argumentos, int));
-				len = 1;
-				break;
-			case 's':
-				s = va_arg(argumentos, char*);
-				if (s == NULL)
-					s = "nil";
-				printf("%s", s);
-				len = 1;
-				break;
-			case 'f':
-				printf("%f", va_arg(argumentos, double));
-				len = 1;
-				break;
-			default:
-				len = 0;
-				break;
+			i = 1;
+			switch (format[j])
+			{
+				case 'c':
+					printf("%c", va_arg(argumentos, int));
+					break;
+				case 'i':
+					printf("%i", va_arg(argumentos, int));
+					break;
+				case 's':
+					s = va_arg(argumentos, char*);
+					if (s == NULL)
+						s = "nil";
+					printf("%s", s);
+					break;
+				case 'f':
+					printf("%f", va_arg(argumentos, double));
+					break;
+				default:
+					i = 0;
+					break;
+			}
+			if (format[j + 1] && i)
+				printf(", ");
+			j++;
 		}
-		if (format[j + 1] && len == 1)
-			printf(", ");
-		j++;
+		putchar(10);
+		va_end(argumentos);
+		break;
 	}
-	putchar(10);
-	va_end(argumentos);
 }
