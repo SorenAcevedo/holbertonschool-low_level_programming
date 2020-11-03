abce3c485a2bf66fa44999fc926d7f05db0d2acb
SorenAcevedo <sorenacevedo@gmail.com>
2020-11-03 15:16:03 +0000

    Create 01 file, count number of elements in a linked list

---
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+
+/**
+* listint_len - print list with int
+* @h: pointer to head of the list
+* Return: return the number of nodes
+*/
+
+size_t listint_len(const listint_t *h)
+{
+	int n;
+
+	while(h)
+	{
+		h  = h->next;
+		n++;
+	}
+	return (n);
+}
