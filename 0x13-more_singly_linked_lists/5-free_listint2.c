dbcaa63fdd0d10b2311b900fe9058205035592f7
SorenAcevedo <sorenacevedo@gmail.com>
2020-11-04 16:34:13 +0000

    Correction 05

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -9,7 +9,10 @@ void free_listint2(listint_t **head)
 {
 	listint_t *next = NULL;
 
-	while (*head && head)
+	if (head == NULL)
+		return;
+
+	while (*head)
 	{
 		next = (*head)->next;
 		free(*head);
