00229d8bde42a26c1cead2066f23d9466e4a0e7e
SorenAcevedo <sorenacevedo@gmail.com>
2020-11-05 16:52:18 +0000

    Create directory

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,30 @@
+#include "holberton.h"
+
+/**
+ * binary_to_uint - convert binary to decimal.
+ * @b: string with binary number.
+ * Return: decimal number, if error 0.
+ */
+
+unsigned int binary_to_uint(const char *b)
+{
+	int len = -1, dec = 0, i, j, pos;
+
+
+	while (b[len + 1])
+		len++;
+
+	for (i = 0; b[i]; i++, len--)
+	{
+		if (b[len] != '0' && b[len] != '1')
+			return (0);
+
+		pos = 1;
+
+		for (j = 0; j < i; j++)
+			pos *= 2;
+
+		dec += (pos * (b[len] - 48)); 
+	}
+	return (dec);
+}
