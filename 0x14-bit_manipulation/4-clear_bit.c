df74cafb28825a9f855c7b7774e80bb54c4f70bc
SorenAcevedo <sorenacevedo@gmail.com>
2020-11-05 21:49:53 +0000

    Create 04

---
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -0,0 +1,22 @@
+#include "holberton.h"
+
+/**
+ * clear_bit - set bit to 0.
+ * @n: dec number.
+ * @index: index.
+ * Return: 1 if it worked, or -1 if fail
+ */
+
+int clear_bit(unsigned long int *n, unsigned int index)
+{
+	int mask = 1;
+
+	if (index > 63)
+		return (-1);
+
+	mask <<= index;
+	mask = ~ mask;
+
+	*n = *n & mask;
+	return (1);
+}
