feaf3f647a3c3a4bd7876416628cf67b3471e1d7
SorenAcevedo <sorenacevedo@gmail.com>
2020-11-05 19:47:40 +0000

    Create 01

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -1,32 +1,32 @@
-#include "holberton.h"
+#include "holberton.h"	
 
-/**
- * print_binary - convert and print dec to bi.
- * @n: dec number.
- */
+/**	
+ * print_binary - convert and print dec to bi.	
+ * @n: dec number.	
+ */	
 
-void print_binary(unsigned long int n)
-{
-	unsigned long int mask = 1, copy = n;
+void print_binary(unsigned long int n)	
+{	
+	unsigned long int mask = 1, copy = n;	
 
-	if (n == 0)
-	{
-		_putchar(48);
-		return;
-	}
-	/* Generar mascara de cantidad de dígitos*/
-	while (n > 1)
-	{
-		n >>= 1;
-		mask <<= 1;
-	}
-	/*recorro número y voy comparando*/
-	while (mask)
-	{
-		if (copy & mask)
-			_putchar(49);
-		else
-			_putchar(48);
-		mask >>= 1;
-	}
+	if (n == 0)	
+	{	
+		_putchar(48);	
+		return;	
+	}	
+	/* Generar mascara de cantidad de dígitos*/	
+	while (n > 1)	
+	{	
+		n >>= 1;	
+		mask <<= 1;	
+	}	
+	/*recorro número y voy comparando*/	
+	while (mask)	
+	{	
+		if (copy & mask)	
+			_putchar(49);	
+		else	
+			_putchar(48);	
+		mask >>= 1;	
+	}	
 }
