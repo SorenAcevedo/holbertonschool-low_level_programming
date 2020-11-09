330cf9c9ff86e164a299468ad8f09492bb6532c2
SorenAcevedo <sorenacevedo@gmail.com>
2020-11-09 19:56:44 +0000

    Create 01

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,25 @@
+#include "holberton.h"
+
+/**
+ * create_file - create a new file.
+ * @filename: file name.
+ * @text_content: pointer..
+ * Return: Number of written chars or 0 if fail.
+ */
+
+int create_file(const char *filename, char *text_content)
+{
+	ssize_t fd, len = 0;
+
+	while (text_content[len])
+		len++;
+
+	fd = open(filename, O_CREAT | O_WRONLY, 0600);
+
+	if (fd != -1 && filename && text_content)
+	{
+		write(fd, text_content, len);
+		close(fd);
+	}
+	return (-1);
+}
