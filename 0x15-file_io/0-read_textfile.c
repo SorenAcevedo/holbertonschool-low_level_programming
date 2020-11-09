61e4dbd27e151b8000336b29f1229188e70bca9c
SorenAcevedo <sorenacevedo@gmail.com>
2020-11-09 18:37:43 +0000

    Correction 00

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -12,25 +12,27 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	ssize_t fd, rfd, wfd;
 	char *buffer;
 
+	if (filename == NULL)
+		return (0);
+
 	buffer = malloc(sizeof(char) * letters);
+	if (!buffer)
+		return (0);
 
 	fd = open(filename, O_RDONLY);
-	
-	if (fd != -1 && buffer != NULL && filename != NULL)
-	{
-		rfd = read(fd, buffer, letters);
+	if (fd == -1)
+		return (0);
 
-		if (rfd == -1)
-			return (0);
+	rfd = read(fd, buffer, letters);
+	if (rfd == -1)
+		return (0);
 
-		close(fd);
-		wfd = write(1, buffer, rfd);
+	close(fd);
+	wfd = write(1, buffer, rfd);
 
-		if (wfd != -1)
-		{
-			free(buffer);
-			return (wfd);
-		}
-	}
-	return (0);
+	if (wfd == -1)
+		return (0);
+
+	free(buffer);
+	return (wfd);
 }
