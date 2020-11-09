8d20a60d4404866381fbe9e8b332be40602c4345
SorenAcevedo <sorenacevedo@gmail.com>
2020-11-09 20:17:41 +0000

    Correction 00

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -14,7 +14,7 @@ int create_file(const char *filename, char *text_content)
 	while (text_content[len])
 		len++;
 
-	fd = open(filename, O_TRUNC | O_CREAT | O_WRONLY, S_IRUSR | S_IWUSR);
+	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC , 0600);
 
 	if (fd != -1 && filename)
 	{
