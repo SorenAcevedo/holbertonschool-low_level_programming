4d6c1823a5610caad5231dcc2302118b11457f39
SorenAcevedo <sorenacevedo@gmail.com>
2020-11-09 19:03:26 +0000

    Correction 00

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -17,7 +17,7 @@ ssize_t read_textfile(const char *filename, size_t letters)
 		return (0);
 
 	buffer = malloc(sizeof(char) * letters);
-	if (!buffer)
+	if (buffer == NULL)
 		return (0);
 
 	fd = open(filename, O_RDONLY);
