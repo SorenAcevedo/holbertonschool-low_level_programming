fbd9e4ba78151adf3a3591389feedc4cc08365fe
SorenAcevedo <sorenacevedo@gmail.com>
2020-11-09 20:00:05 +0000

    Correction 01

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -20,7 +20,8 @@ int create_file(const char *filename, char *text_content)
 	{
 		write(fd, text_content, len);
 		close(fd);
-		return (1);
+		if (write != -1)
+			return (1);
 	}
 	return (-1);
 }
