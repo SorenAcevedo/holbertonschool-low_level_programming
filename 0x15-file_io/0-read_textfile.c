21eef8b94c1f157a3dbde7c142aa031c1e792313
SorenAcevedo <sorenacevedo@gmail.com>
2020-11-09 18:14:00 +0000

    Correction 00

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -19,18 +19,11 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	if (fd != -1 && filename && buffer)
 	{
 		rfd = read(fd, buffer, letters);
+		wfd = write(1, buffer, rfd);
+		close(fd);
 
-		if (rfd != -1)
-		{
-			wfd = write(1, buffer, rfd);
-			close(fd);
-
-			if (wfd != -1)
-			{
-			free(buffer);
+		if (wfd != -1)
 			return (wfd);
-			}
-		}
 	}
 	free(buffer);
 	return (0);
