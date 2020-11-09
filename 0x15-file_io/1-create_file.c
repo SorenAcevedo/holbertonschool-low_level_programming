53bf4bf0ae55e0ab490be8f462b188f57f6aed4b
SorenAcevedo <sorenacevedo@gmail.com>
2020-11-09 20:38:22 +0000

    Correction 01

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -11,23 +11,20 @@ int create_file(const char *filename, char *text_content)
 {
 	ssize_t len, fd;
 
-	if (!filename)
-		return (-1);
-
 	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
-	if (fd == -1)
-		return (-1);
-
-	if (text_content)
+	if (fd != -1 && filename)
 	{
-		len = 0;
+		if (text_content)
+		{
+			len = 0;
 
-		while (text_content[len])
-			len++;
+			while (text_content[len])
+				len++;
+		}
+		write(fd, text_content, len);
+		close(fd);
+		return (1);
 	}
-	write(fd, text_content, len);
-
 	close(fd);
-	return (1);
-
+	return (-1);
 }
