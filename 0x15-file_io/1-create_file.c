9471b948dab2fd6927c7d2cc772087c54ef88bfe
SorenAcevedo <sorenacevedo@gmail.com>
2020-11-09 20:03:27 +0000

    Correction 01

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -16,7 +16,7 @@ int create_file(const char *filename, char *text_content)
 
 	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
 
-	if (fd != -1 && filename && text_content)
+	if (fd != -1 && filename)
 	{
 		wfd = write(fd, text_content, len);
 		close(fd);
