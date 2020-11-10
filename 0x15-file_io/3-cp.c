6ce7db0acd44f9c33ab70432f88046f505f9e7be
SorenAcevedo <sorenacevedo@gmail.com>
2020-11-10 00:26:20 +0000

    Correction 03

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -16,23 +16,24 @@ int main(int ac, char **av)
 		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n"), exit(97);
 
 	fd1 = open(av[1], O_RDONLY);
-	fd2 = open(av[2], O_CREAT | O_RDWR | O_TRUNC, 0664);
+	if (fd1 == -1)
+		dprintf(STDERR_FILENO, "Error: Can't write to %s", av[1]), exit(98);
+
+	fd2 = open(av[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
+	if (fd2 == -1)
+		dprintf(STDERR_FILENO, "Error: Can't write to %s", av[2]), exit(99);
 
 	if (fd1 != -1 && fd2 != -1)
 	{
 		for (rfd1 = read(fd1, buf, size); rfd1 > 0; rfd1 = read(fd1, buf, size))
 		{
+			if (rfd1 == -1)
+				dprintf(STDERR_FILENO, "Error: Can't write to %s", av[1]), exit(98);
 			wfd2 = write(fd2, buf, rfd1);
 			if (wfd2 == -1)
-				break;
+				dprintf(STDERR_FILENO, "Error: Can't write to %s", av[2]), exit(99);
 		}
 	}
-
-	if (fd1 == -1 || rfd1 == -1)
-		dprintf(STDERR_FILENO, "Error: Can't write to %s", av[1]), exit(98);
-
-	if (fd2 == -1 || wfd2 == -1)
-		dprintf(STDERR_FILENO, "Error: Can't write to %s", av[2]), exit(99);
 
 	if (close(fd1) == -1)
 		dprintf(STDERR_FILENO, "Error: Can't close fd %d", fd1), exit(100);
