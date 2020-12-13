64ceb4bdf61540b8544031939158e1c85cf38645
SorenAcevedo <sorenacevedo@gmail.com>
2020-12-13 10:32:25 -0500

    Add null condition

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -32,7 +32,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 
 	while (i < (idx - 1))
 	{
-		if (temp == NULL)
+		if (temp->next == NULL)
 		{
 			free(new);
 			return (NULL);
