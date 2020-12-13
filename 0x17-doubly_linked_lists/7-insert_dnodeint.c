6cc39c1bcdd54c5c286c4c6b8f9b11639e17d130
SorenAcevedo <sorenacevedo@gmail.com>
2020-12-13 11:55:01 -0500

    Add functions in file

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -2,6 +2,65 @@
 #include <stdlib.h>
 
 /**
+ * add_dnodeint - add node at start
+ * @head: pointer to head
+ * @n: number of the new node
+ * Return: new node
+ */
+
+dlistint_t *add_dnodeint(dlistint_t **head, const int n)
+{
+	dlistint_t *new;
+
+	new = malloc(sizeof(dlistint_t));
+	if (new == NULL)
+		return (NULL);
+	new->n = n;
+	new->prev = NULL;
+	if (*head)
+	{
+		(*head)->prev = new;
+		new->next = *head;
+	}
+	else
+		new->next = NULL;
+	*head = new;
+	return (new);
+}
+
+/**
+ * add_dnodeint_end - add node at the end
+ * @head: pointer to head
+ * @n: number of the new node
+ * Return: new node
+ */
+
+dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
+{
+	dlistint_t *new, *temp;
+
+	new = malloc(sizeof(dlistint_t));
+	if (new == NULL)
+		return (NULL);
+	new->n = n;
+	new->next = NULL;
+	if (!*head)
+	{
+		new->prev = NULL;
+		*head = new;
+		return (new);
+	}
+	temp = *head;
+	while (temp->next)
+	{
+		temp = temp->next;
+	}
+	temp->next = new;
+	new->prev = temp;
+	return (new);
+}
+
+/**
  * insert_dnodeint_at_index - insert node at index
  * @h: pointer to head
  * @idx: index
