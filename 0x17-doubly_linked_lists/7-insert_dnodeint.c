8e79e2ba403761b6b833696de0892768ac85aaad
SorenAcevedo <sorenacevedo@gmail.com>
2020-12-13 11:23:32 -0500

    Add idx at start and at the end, using the 2° and 3° functions

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -20,19 +20,13 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 		return (NULL);
 	new->n = n;
 
-	if (*h == NULL)
-	{
-		new->prev = NULL;
-		new->next = NULL;
-		*h = new;
-		return (new);
-	}
+	if (idx == 0)
+		return (add_dnodeint(h, n));
 
 	temp = *h;
-
 	while (i < (idx - 1))
 	{
-		if (temp->next == NULL)
+		if (temp == NULL)
 		{
 			free(new);
 			return (NULL);
@@ -41,10 +35,12 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 		i++;
 	}
 
+	if (i == idx - 1)
+		return (add_dnodeint_end(h, n));
+
 	temp->next->prev = new;
 	new->next = temp->next;
 	temp->next = new;
 	new->prev = temp;
-
 	return (new);
 }
