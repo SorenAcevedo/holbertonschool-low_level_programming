ea82c16265f7cd255c3de14ab74085861681716c
SorenAcevedo <sorenacevedo@gmail.com>
2021-01-15 11:45:23 -0500

    Correction 04

--- 0x1A-hash_tables/4-hash_table_get.c
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -15,15 +15,17 @@ char *hash_table_get(const hash_table_t *ht, const char *key)
 	hash_node_t *temp;
 	unsigned long int idx;
 
-	idx = key_index((unsigned const char *) key, ht->size);
-
-	temp = ht->array[idx];
-
-	while (temp)
+	if (ht && key)
 	{
-		if (strcmp(temp->key, key) == 0)
-			return (temp->value);
-		temp = temp->next;
+		idx = key_index((unsigned const char *) key, ht->size);
+		temp = ht->array[idx];
+
+		while (temp)
+		{
+			if (strcmp(temp->key, key) == 0)
+				return (temp->value);
+			temp = temp->next;
+		}
 	}
 	return (NULL);
 }
