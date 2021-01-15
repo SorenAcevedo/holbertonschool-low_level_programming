fdf0795583e64cc7cf91a8aaac8aaaf0ef2cff86
SorenAcevedo <sorenacevedo@gmail.com>
2021-01-15 12:24:40 -0500

    Correction 03

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -45,7 +45,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	}
 	new->value = strdup(value);
 	new->key = strdup(key);
-	new->next = ht->array[idx]->next;
+	new->next = ht->array[idx];
 	ht->array[idx] = new;
 	return (1);
 }
