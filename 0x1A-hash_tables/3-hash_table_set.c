d8591e58d305805f2c8444ba527979ebd1bbae72
SorenAcevedo <sorenacevedo@gmail.com>
2021-01-15 11:08:18 -0500

    Add 03

---
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -0,0 +1,48 @@
+#include "hash_tables.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ * hash_table_set - set key at index in a new node
+ * @ht: hash table.
+ * @key: key to include at index
+ * @value: value to include at index
+ * Return: 1 if it succeeded, 0 otherwise
+ */
+
+int hash_table_set(hash_table_t *ht, const char *key, const char *value)
+{
+	hash_node_t *new, *temp;
+	unsigned long int idx;
+
+	idx = key_index((unsigned const char *) key, ht->size);
+
+	new = malloc(sizeof(hash_node_t));
+	if (!new)
+		return (0);
+
+	if (ht->array[idx] == NULL)
+	{
+		new->value = strdup(value);
+		new->key = strdup(key);
+		new->next = NULL;
+		ht->array[idx] = new;
+		return (1);
+	}
+	temp = ht->array[idx];
+	while (temp)
+	{
+		if (strcmp(temp->key, key) == 0)
+		{
+			temp->value = strdup(value);
+			free(new);
+			return (1);
+		}
+	}
+	new->value = strdup(value);
+	new->key = strdup(key);
+	new->next = ht->array[idx]->next;
+	ht->array[idx] = new;
+	return (1);
+}
