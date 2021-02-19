4f09aa6cc33d0418293219454a846ad4570a188a
SorenAcevedo <sorenacevedo@gmail.com>
2021-02-19 11:58:02 -0500

    Add files

---
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+""" Island perimeter """
+
+
+def island_perimeter(grid):
+    """ Calculate island perimeter """
+
+    perimeter = 0
+
+    for i in range(len(grid)):
+        for j in range(len(grid[0])):
+            if grid[i][j] == 1:
+                perimeter += 4
+                try:
+                    if grid[i][j + 1] == 1:
+                        perimeter -= 1
+                except:
+                    pass
+                try:
+                    if grid[i][j - 1] == 1:
+                        perimeter -= 1
+                except:
+                    pass
+                try:
+                    if grid[i + 1][j] == 1:
+                        perimeter -= 1
+                except:
+                    pass
+                try:
+                    if grid[i - 1][j] == 1:
+                        perimeter -= 1
+                except:
+                    pass
+    return perimeter
