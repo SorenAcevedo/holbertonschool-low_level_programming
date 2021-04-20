5a76d9d7b77000d5d84b07de569e6cc2a1f57858
SorenAcevedo <sorenacevedo@gmail.com>
2021-04-20 16:40:37 -0500

    Add condition with NULL array

--- 0x1E-search_algorithms/1-binary.c
+++ 0x1E-search_algorithms/1-binary.c
@@ -18,7 +18,7 @@ int binary_search(int *array, size_t size, int value)
 	l = 0;
 	r = size - 1;
 
-	while (l <= r || array)
+	while (l <= r && array)
 	{
 		sep = "";
 		printf("Searching in array:");
