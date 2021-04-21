8221bdc4815928caf5ef75afa4f240ae70171a1d
SorenAcevedo <sorenacevedo@gmail.com>
2021-04-21 11:10:30 -0500

    Add advance

--- 0x1E-search_algorithms/100-jump.c
+++ 0x1E-search_algorithms/100-jump.c
@@ -25,9 +25,7 @@ int jump_search(int *array, size_t size, int value)
 		{
 			printf("Value checked array[%d] = [%d]\n", i, i);
 			if (array[i] == value)
-			{
 				break;
-			}
 			i += jump;
 		}
 		printf("Value found between indexes [%d] and [%d]\n", i - 3, i);
