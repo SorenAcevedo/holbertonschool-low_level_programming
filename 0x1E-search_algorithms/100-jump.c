6f288dec5268a9777d40e526537616834780e8d4
SorenAcevedo <sorenacevedo@gmail.com>
2021-04-21 11:42:35 -0500

    Change condition

--- 0x1E-search_algorithms/100-jump.c
+++ 0x1E-search_algorithms/100-jump.c
@@ -22,7 +22,7 @@ int jump_search(int *array, size_t size, int value)
 	if (!array)
 		return (-1);
 
-	while ((array[i] <= value) && (i <= size))
+	while ((array[i] <= value) && (i < size))
 	{
 		if (array[i] == value)
 			break;
